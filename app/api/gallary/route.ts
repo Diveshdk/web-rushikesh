6140671ab8d0902bea70673d6e90f6faac30425d
Bhavesh <bhavesh@valura.ai>
2026-04-26 16:43:47 +0530

    feat: remove unused gallery API route

--- app/api/gallary/route.ts
+++
@@ -1,6 +0,0 @@
-import { prisma } from '../../lib/prisma';
-
-export async function GET() {
-  const posts = await prisma.images.findMany().then((res) => res.map((item) => item.url));
-  return Response.json(posts)
-}
